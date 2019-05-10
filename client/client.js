cdef644b8113ed77c2a6682603f80690bfdaf5c7
matcool <26722564+matcool@users.noreply.github.com>
2019-05-10 20:39:18 -0300

    Profile pictures
    Hardcoded one for now

--- client/client.js
+++ client/client.js
@@ -1,10 +1,17 @@
 const socket = io();
 
+const imgpfp = 'https://images.unsplash.com/photo-1556220881-df28b44798ce?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=100&h=100&fit=crop&ixid=eyJhcHBfaWQiOjF9';
+
 Vue.component('message', {
     props: ['message'],
-    template: `<div><span class="message-nickname">{{ message.author }}</span>
-    <span class="message-time">{{ message.timestampstr }}</span><br>
-    <span class="message-text">{{ message.text }}</span></div>`
+    template: `<div class="message-parent"><img class="message-pfp" src="${imgpfp}"><div><span class="message-nickname">{{ message.author }}</span>\
+<span class="message-time">{{ timestampstr }}</span><br>
+<span class="message-text">{{ message.text }}</span></div></div>`,
+    computed: {
+        timestampstr: function() {
+            return (new Date(this.message.timestamp)).toDateString();
+        }
+    }
 });
 
 const messages = new Vue({
@@ -15,12 +22,7 @@ const messages = new Vue({
     }
 });
 
-function fancyTime(time) {
-    return (new Date(time)).toDateString();
-}
-
 function addMessage(msg) {
-    msg.timestampstr = fancyTime(msg.timestamp);
     messages.messages.push(msg);
 }
 
