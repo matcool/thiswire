fd4e2c00667fce58b049c92b888103bbb1bce87f
matcool <26722564+matcool@users.noreply.github.com>
2019-05-13 18:18:28 -0300

    Simple user system and markdown
    Currently just saves all the users that have logged in in an array, let's
    you rejoin by checking if theres a user with the same name and if it's
    not logged in
    Markdown is done with markdown-it, still need to figure out how to
    change some stuff (like __test__ should be underline not bold)

--- client/client.js
+++ client/client.js
@@ -1,15 +1,22 @@
 const socket = io();
+const md = window.markdownit({
+    breaks: true,
+    linkify: true
+});
 
 const imgpfp = 'https://images.unsplash.com/photo-1556220881-df28b44798ce?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=100&h=100&fit=crop&ixid=eyJhcHBfaWQiOjF9';
 
 Vue.component('message', {
     props: ['message'],
-    template: `<div class="message-parent"><img class="message-pfp" src="${imgpfp}"><div><span class="message-nickname">{{ message.author }}</span>\
+    template: `<div class="message-parent"><img class="message-pfp" src="${imgpfp}"><div><span class="message-nickname">{{ message.author.name }}</span>\
 <span class="message-time">{{ timestampstr }}</span><br>
-<span class="message-text">{{ message.text }}</span></div></div>`,
+<div class="message-text" v-html="content"></div></div></div>`,
     computed: {
         timestampstr: function() {
             return (new Date(this.message.timestamp)).toDateString();
+        },
+        content: function() {
+            return md.renderInline(this.message.text);
         }
     }
 });
@@ -30,20 +37,37 @@ socket.on('chat message', (msg) => {
     addMessage(msg);
 });
 
+socket.on('chat messages', (msgs) => {
+    msgs.map(msg => addMessage(msg));
+});
+
 const app = new Vue({
     el: '#message-sender',
     data: {
         message: '',
-        nickname: 'Joe'
+        loggedIn: false,
+        user: {
+            name: ''
+        }
     },
     methods: {
         sendMessage: function () {
-            if (this.message == '' || this.nickname == '') return;
+            if (this.message == '') return;
             socket.emit('chat message', {
                 text: this.message,
-                author: this.nickname
+                author: this.user.id
             });
             this.message = '';
+        },
+        login: function () {
+            if (this.user.name == '') return;
+            socket.emit('login', this.user);
         }
     }
+});
+
+socket.on('login', (user) => {
+    if (user == null) return;
+    app.loggedIn = true;
+    app.user = user;
 });
--- server.js
+++ server.js
@@ -5,32 +5,90 @@ let app = express();
 let http = require('http').Server(app);
 let io = socketio(http);
 
-let id = 0;
+let msgid = 0;
 const messages = [];
+let userid = 0;
+const users = [];
 
 function addMessage(msg) {
     msg = {
-        id: id,
+        id: msgid,
         text: msg.text,
-        author: msg.author,
+        author: users[msg.author],
         timestamp: new Date()
     };
     messages.push(msg);
-    id++;
+    msgid++;
     return msg;
+}
+
+function addUser(user) {
+    user = {
+        id: userid,
+        name: user.name,
+        loggedIn: true
+    };
+    users.push(user);
+    userid++;
+    return user;
+}
+
+function findUser(atr) {
+    for (let i = 0; i < users.length; i++) {
+        let temp = true;
+        for (let key of Reflect.ownKeys(atr)) {
+            if (users[i][key] != atr[key]) {
+                temp = false;
+                break;
+            }
+        }
+        if (temp) return users[i];
+    }
+    return null;
 }
 
 app.use(express.static('client'));
 
 io.on('connection', (socket) => {
     console.log('New connection');
-    messages.forEach(msg => io.emit('chat message', msg));
-    socket.on('chat message', (msg) => {
-        console.log('new message!\n'+JSON.stringify(msg, undefined, 4));
-        io.emit('chat message', addMessage(msg));
+    let id;
+    socket.on('login', (user) => {
+        // Look for another user with same name and check if logged in
+        let usr = findUser({
+            name: user.name
+        });
+        if (usr == null) {
+            console.log('new user! ' + JSON.stringify(user, undefined, 4));
+            user = addUser(user);
+        } else if (usr.loggedIn) {
+            io.emit('login', null);
+            return;
+        } else if (!usr.loggedIn) {
+            console.log(user.name + ' logged back in');
+            user = usr;
+        }
+
+        id = user.id;
+        io.emit('login', user);
+
+        io.emit('chat messages', messages);
+        socket.on('chat message', (msg) => {
+            console.log('new message! ' + JSON.stringify(msg, undefined, 4));
+            io.emit('chat message', addMessage(msg));
+        });
     });
+
     socket.on('disconnect', () => {
         console.log('Disconnected');
+        // Set loggedIn to false when disconnected
+        if (id !== null) {
+            let usr = findUser({
+                id: id
+            });
+            if (usr != null) {
+                usr.loggedIn = false;
+            }
+        }
         io.emit('user disconnected');
     });
 });
