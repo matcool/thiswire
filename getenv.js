d788cd6f518395417c4d61674e69a5ddc7f14f20
matcool <26722564+matcool@users.noreply.github.com>
2019-08-30 18:21:15 -0300

    Switch to .env.local

--- getenv.js
+++ getenv.js
@@ -1,4 +1,7 @@
-let env = require('dotenv').config({});
+const exists = require('fs').existsSync;
+let path = './.env.local';
+if (!exists(path)) path = './.env';
+let env = require('dotenv').config({path});
 if (env.error) throw env.error;
 env = require('dotenv-parse-variables')(env.parsed);
 Object.freeze(env);
